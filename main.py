6de0e12254db55f1c6c53af51e7a7350739ec91e
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-03-21 20:04:10 +0600

    Add daily data using symbol and market.

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ token_telegram = os.environ.get('TELEGRAME_TOKEN')
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! I am your trading bot. '
                               'Use /trade to execute a mock trade.'
-                              'Use /price to execute price')
+                              'Use /price to execute price.'
+                              "Use '/dailydata' to execute daily data.")
 
 def trade(update: Update, context: CallbackContext) -> None:
     data = get_market_data()
@@ -48,14 +49,42 @@ def price(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(message)
 
 
-def main():
-    token = '7140481417:AAHGrdKjCCbMguXFIoUaSsuPDzRRaGEN-gI'
-    print(token)
-    print(token_telegram)
-    if token==token_telegram:
-        print('true')
+def get_daily_crypto_data(api_key, symbol='BTC', market='USD'):
+    url = f'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market={market}&apikey={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    
+    try:
+        today_date = data['Meta Data']['6. Last Refreshed'].split()[0]
+        
+        today_data = data['Time Series (Digital Currency Daily)'][today_date]
+        return today_data
+    except KeyError:
+        return None
+    
+def daily_data(update: Update, context: CallbackContext) -> None:
+    api_key = os.getenv('ALPHA_API')  
+    args = context.args
+    
+    if len(args) >= 2:
+        symbol = args[0].upper()
+        market = args[1].upper()
     else:
-        print('false')
+        update.message.reply_text("Please provide the symbol and market. Usage: /dailydata <SYMBOL> <MARKET>")
+        return
+    
+    data = get_daily_crypto_data(api_key, symbol, market)
+    if data:
+        # Format the message with the desired information
+        message = f"Today's {symbol} Data in {market}:\nOpen: {data.get('1a. open (' + market + ')', 'N/A')}\nHigh: {data.get('2a. high (' + market + ')', 'N/A')}\nLow: {data.get('3a. low (' + market + ')', 'N/A')}\nClose: {data.get('4a. close (' + market + ')', 'N/A')}"
+    else:
+        message = "Data is currently unavailable. Please try again later or check the symbol and market."
+    
+    update.message.reply_text(message)
+
+
+
+def main():
     updater = Updater(token_telegram)
 
     print("Bot started")
@@ -65,6 +94,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("trade", trade))
     dp.add_handler(CommandHandler("price", price))
+    dp.add_handler(CommandHandler("dailydata", daily_data))
 
 
     updater.start_polling()
