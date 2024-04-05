70a5789d96c5b5d070e7dcdbde36db3a6d9300ca
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-04-06 02:52:02 +0600

    Update gitginore.

--- new.py
+++
@@ -1,7 +0,0 @@
-from check_account import check_account_wallet
-
-
-api_key = 'RoaGKZHSuwisbhGf'
-api_secret = 'X1B35w8h0CqLmBB83vKZnj46lLaRpFnU'
-
-check_account_wallet(api_key=api_key, api_secret=api_secret, product_id='BTC-USDC')
