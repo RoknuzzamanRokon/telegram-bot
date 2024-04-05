9b3a802727afb5adfaf1d4d4c9219b00f297c791
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-04-06 01:13:00 +0600

    Update check account info function.

--- check_account.py
+++ check_account.py
@@ -21,9 +21,27 @@ from coinbase.wallet.client import Client
 #     except Exception as e:
 #         print("Error:", e)
 
+# def check_account_wallet(api_key, api_secret, product_id=None):
+#     client = Client(api_key, api_secret)
+#     currencies = product_id.split('-') if product_id else []
+
+#     try:
+#         accounts = client.get_accounts()
+#         for account in accounts['data']:
+#             balance_amount = float(account['balance']['amount'])
+#             account_currency = account['balance']['currency']
+#             if balance_amount > 0 and (not product_id or account_currency in currencies):
+#                 print(f"A\\C: {account['name']}, Balance: {balance_amount} {account_currency}")
+#     except Exception as e:
+#         print("Error:", e)
+
+
+
+
 def check_account_wallet(api_key, api_secret, product_id=None):
     client = Client(api_key, api_secret)
     currencies = product_id.split('-') if product_id else []
+    balances_info = "" 
 
     try:
         accounts = client.get_accounts()
@@ -31,6 +49,12 @@ def check_account_wallet(api_key, api_secret, product_id=None):
             balance_amount = float(account['balance']['amount'])
             account_currency = account['balance']['currency']
             if balance_amount > 0 and (not product_id or account_currency in currencies):
-                print(f"A\\C: {account['name']}, Balance: {balance_amount} {account_currency}")
+                account_info = f"A\\C: {account['name']}, Balance: {balance_amount} {account_currency}\n"
+                print(account_info)  
+                balances_info += account_info  
     except Exception as e:
         print("Error:", e)
+        balances_info += f"Error retrieving account information: {e}\n"  
+
+    return balances_info.strip()  
+
